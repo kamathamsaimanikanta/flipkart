35cb8269e192fe895748f36cb3a1b59bd7ccf8ee
kamathamsaimanikanta <kamathamsaimanikanta@gmail.com>
2025-07-15 12:32:27 +0530

    adding file

--- flipkart6.java
+++ flipkart6.java
@@ -2,7 +2,8 @@ public class Flipkart6
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Flipkart6")
+		System.out.println("Flipkart6");
+                System.out.println("Flipkart6A");
 	}
 }
 
